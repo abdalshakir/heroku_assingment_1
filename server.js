9ac03a5a3c3a049fbcd00b32589af91237f7119b
Abdal Shakir <64357110+abdalshakir@users.noreply.github.com>
2021-10-29 22:23:18 +0500

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = process.env.port || 
+const port = process.env.PORT || 3000
  
 app.get('/', function (req, res) {
   res.send('Hello World')
